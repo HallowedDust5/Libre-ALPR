87e7f8fac5f4d527ae32799e14c70f07fbecbd23
Eizaku Asai <vmr4ku@virginia.edu>
2022-10-23 13:59:43 -0400

    changes

--- cv2ml.py
+++ cv2ml.py
@@ -6,18 +6,24 @@ import easyocr
 
 past_result = None
 result = None
+show_plots = False
+show = input("type y to show images")
+if show == 'y':
+    show_plots = True
 for i in range(104):
     curNumber = i+1
     image = r'carswithlicense\images - 2022-10-23T001656.992 (' +  str(curNumber) + ').jpg'
     img = cv2.imread(image)
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    plt.imshow(cv2.cvtColor(gray, cv2.COLOR_BGR2RGB))
-    plt.show()
+    if show_plots:
+        plt.imshow(cv2.cvtColor(gray, cv2.COLOR_BGR2RGB))
+        plt.show()
 
     bfilter = cv2.bilateralFilter(gray, 11, 17, 17) #Noise reduction
-    edged = cv2.Canny(bfilter, 230, 330) #Edge detection
-    plt.imshow(cv2.cvtColor(edged, cv2.COLOR_BGR2RGB))
-    plt.show()
+    edged = cv2.Canny(bfilter, 230, 300) #Edge detection
+    if show_plots:
+        plt.imshow(cv2.cvtColor(edged, cv2.COLOR_BGR2RGB))
+        plt.show()
 
     keypoints = cv2.findContours(edged.copy(), cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
     contours = imutils.grab_contours(keypoints)
@@ -34,16 +40,17 @@ for i in range(104):
     try:
         new_image = cv2.drawContours(mask, [location], 0,255, -1)
         new_image = cv2.bitwise_and(img, img, mask=mask)
-        plt.imshow(cv2.cvtColor(new_image, cv2.COLOR_BGR2RGB))
-        plt.show()
+        if show_plots:
+            plt.imshow(cv2.cvtColor(new_image, cv2.COLOR_BGR2RGB))
+            plt.show()
 
         (x,y) = np.where(mask==255)
         (x1, y1) = (np.min(x), np.min(y))
         (x2, y2) = (np.max(x), np.max(y))
         cropped_image = gray[x1:x2+1, y1:y2+1]
-
-        plt.imshow(cv2.cvtColor(cropped_image, cv2.COLOR_BGR2RGB))
-        plt.show()
+        if show_plots:
+            plt.imshow(cv2.cvtColor(cropped_image, cv2.COLOR_BGR2RGB))
+            plt.show()
     except:
         print("contours failed")
 
@@ -54,8 +61,9 @@ for i in range(104):
         font = cv2.FONT_HERSHEY_SIMPLEX
         res = cv2.putText(img, text=text, org=(approx[0][0][0], approx[1][0][1]+60), fontFace=font, fontScale=1, color=(0,255,0), thickness=2, lineType=cv2.LINE_AA)            
         res = cv2.rectangle(img, tuple(approx[0][0]), tuple(approx[2][0]), (0,255,0),3)
-        plt.imshow(cv2.cvtColor(res, cv2.COLOR_BGR2RGB))
-        plt.show()
+        if show_plots:
+            plt.imshow(cv2.cvtColor(res, cv2.COLOR_BGR2RGB))
+            plt.show()
         print(result[0][1])
     except:
         print("Text not detected")
@@ -63,8 +71,8 @@ for i in range(104):
         print(result)
     past_result = result
 
-    input = ("q to quit, none for next")
-    if input == 'q':
+    quit = input("q to quit, enter for next")
+    if quit == 'q':
         break
 
 print('finished')
