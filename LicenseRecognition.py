6c144901fba0d334258680c034c73e1158bd4aec
Eizaku Asai <vmr4ku@virginia.edu>
2022-10-29 20:55:58 -0400

    changes

--- LicenseRecognition.py
+++ LicenseRecognition.py
@@ -1,16 +1,16 @@
-# from detecto.core import Model, Dataset
-# from detecto import utils, visualize
+from detecto.core import Model, Dataset
+from detecto import utils, visualize
+from detecto.utils import xml_to_csv
 
-# file_loc = r'custom_dataset2/'
-# print(file_loc)
-# dataset = Dataset(file_loc)
-# print(len(dataset))
-# model = Model(['car', 'carswithlicense'])
-# model.fit(dataset)
+xml_to_csv('your_labels/', 'validation-annotations-box.csv')
+dataset = Dataset('validation-annotations-bbox.csv', 'Vehicleregistrationplate/')
+model = Model(['license'])
+model.fit(dataset)
 
-# image = utils.read_image('images - 2022-10-23T001656.992 (20).jpg')
-# cars, carswithlicense = model.predict(image)
-# print(cars)
-# visualize.show_labed_image(image, cars, carswithlicense)
+image = utils.read_image('Vehicleregistrationplate/0c756c9366a8cb10.jpg')
+license = model.predict(image)
+print(cars)
+visualize.show_labed_image(image, license)
+visualize.detect_live(model)
 
 
